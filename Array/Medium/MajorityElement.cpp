cda07ac8383febf27e2e8fbc780c7a2d5dd275f1
Tripsy-Ravi <tripathiravi207@gmail.com>
2026-01-11 03:01:30 -0500

    Majority Element in an array

---
+++ Array/Medium/MajorityElement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Optimized Approach - Iteration 1
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public:
+        int majorityElement(vector<int>& nums) {
+            int num = nums[0];
+            int count = 1;
+            for (int i = 1;i < nums.size();i++) {
+                if (nums[i] == num) {
+                    count++;
+                }else {
+                    count--;
+                    if(count == 0) {
+                        num = nums[i];
+                        count = 1;
+                    }
+                }
+            }
+            return num;
+        }
+    };
