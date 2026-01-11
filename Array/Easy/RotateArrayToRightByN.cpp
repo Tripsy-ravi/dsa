b8bf21522cfdd4483032eaae02a9301286b70fc0
Tripsy-Ravi <tripathiravi207@gmail.com>
2026-01-11 00:44:29 -0500

    Rotate Array to right by n

---
+++ Array/Easy/RotateArrayToRightByN.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute Force Approach - Iteration 1
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public:
+        void rotate(vector<int>& nums, int k) {
+            vector<int> res(nums.size(), 0);
+            for (int i = 0;i < nums.size();i++) {
+                res[(i + k)%nums.size()] = nums[i];
+            }
+            for (int i = 0;i < res.size();i++) {
+                nums[i] = res[i];
+            }
+        }
+    };
